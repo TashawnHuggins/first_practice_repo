70ccdd83f16a37cd394106c9fde1542cc515d6d3
Tashawn <tashawnhuggins@gmail.com>
2022-03-11 15:01:29 -0500

    Entities created

---
+++ Entities/BankAccountClass.py
@@ -0,0 +1,5 @@
+class Account:
+    def __init__(self, customer_id, account_number, account_balance):
+        self.customer_id = customer_id  #  this is used to make instance fields for the object
+        self.account_number = account_number
+        self.account_balance = account_balance
